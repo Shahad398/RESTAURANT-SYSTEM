e7419c8798000f1ec1901c724ece4bca053d0aa1
Shahad398 <belle3e2@gmail.com>
2025-03-03 04:16:57 +0300

    Update RestaurantSystem.java

--- RestaurantSystem.java
+++ RestaurantSystem.java
@@ -65,15 +65,77 @@ System.out.println("3. View previous requests");
 
             }
             break;
-            
+            case 2:
+            	System.out.println("\nDrink Options:");
+                System.out.println("1. Soft Drink");
+                System.out.println("2. Fresh Juice");
+                System.out.print("Choose an option: ");
+                int drinkChoice = input.nextInt();
+                MenuItem drink = null;
+                if (drinkChoice == 1) {
+                	System.out.println("1. 7UP");
+                    System.out.println("2. Pepsi");
+                    System.out.println("3. Diet Pepsi");
+                    System.out.println("4. Mountain Dew");
+                    System.out.print("Your choice: ");
+                    String sodaChoice = input.next();
+                    System.out.print("Enter size (1: Small, 2: Medium, 3: Large): ");
+                    String size = input.next();
+                    double price = (size.equals("1")) ? 5 : (size.equals("2")) ? 7 : 10;
+                     drink = new softDrinks(200,"Soft Drink", price,size,sodaChoice);
+                    order.addItem(drink);
+
+                } else if (drinkChoice == 2) {
+                	System.out.println("1. Orange Juice");
+                    System.out.println("2. Mango Juice");
+                    System.out.println("3. Strawberry Juice");
+                    System.out.print("Your choice: ");
+                    String juiceChoice = input.next();
+                    System.out.print("Enter size (1: Small, 2: Medium, 3: Large): ");
+                    String size = input.next();
+                    double price = (size.equals("1")) ? 7 : (size.equals("2")) ? 10 : 13;
+                    MenuItem juice = new Juices(150,"Natural Juice", price,size,juiceChoice);
+                    order.addItem(juice);
+                }
+                break;
+		case 3:
+            	System.out.println("\nAvailable Ice Cream: Vanilla, Chocolate, Strawberry - 12 SAR");
+            	String IceCream=input.next();
+            	DessertItem sweetItem = new DessertItem(350,"Ice Cream", 12,IceCream);
+            	break;
+	        case 4:
+            	System.out.println("\nValue Meals:");
+                System.out.println("1. Beef Burger Meal - 22 SAR");
+                System.out.println("2. Chicken Burger Meal - 20 SAR");
+                System.out.println("3. Cheese Burger Meal - 18 SAR");
+                System.out.print("Choose a meal: ");
+                int mealChoice = input.nextInt();
+
+
+
+		break;
+		case 5:
+            	 ordering = false;
+                 break;	    
             }//switch (orderChoice)
             
     	}//while(ordering )
+	  customer.placeOrder(order);
+        System.out.println("\nOrder Completed. Here is your receipt:");
+       order.displayOrder();
+	    
     }//if(choice==1)
-    if(choice==3)
-	System.out.println(customer.toString());    
+	 else if (choice == 2) {
+        System.out.println("Order Canceled.");
+        order.clearOrder();
+   else if(choice==3){
+	System.out.println(customer.toString());
+   }
+	else if (choice == 3) {
+        System.out.println("Exiting Program. Goodbye!");
+        break;
 }//while (true)
-
+input.close();
 
 	}
 
